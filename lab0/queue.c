27acb1733c60cf8423a61d60e0730a7186351e09
wd30x <372338552@qq.com>
2021-01-03 13:09:28 +1300

    implement new and free

--- lab0/queue.c
+++ lab0/queue.c
@@ -1,14 +1,14 @@
-/* 
+/*
  * Code for basic C skills diagnostic.
  * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
  */
 
-/*
- * This program implements a queue supporting both FIFO and LIFO
- * operations.
- *
- * It uses a singly-linked list to represent the set of queue elements
- */
+ /*
+  * This program implements a queue supporting both FIFO and LIFO
+  * operations.
+  *
+  * It uses a singly-linked list to represent the set of queue elements
+  */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,20 +16,42 @@
 #include "harness.h"
 #include "queue.h"
 
-/*
-  Create empty queue.
-  Return NULL if could not allocate space.
-*/
-queue_t *q_new()
+  /*
+    Create empty queue.
+    Return NULL if could not allocate space.
+  */
+queue_t* q_new()
 {
-    /* Remember to handle the case if malloc returned NULL */
-    return NULL;
+  /* Remember to handle the case if malloc returned NULL */
+  queue_t* q;
+
+  q = malloc(sizeof(queue_t));
+  if (q == NULL) {
+    return q;
+  }
+  q->head = NULL;
+  q->tail = NULL;
+  q->size = 0;
+  return q;
 }
 
 /* Free all storage used by queue */
-void q_free(queue_t *q)
+void q_free(queue_t* q)
 {
-    /* Remember to free the queue structue and list elements */
+  /* Remember to free the queue structue and list elements */
+  if (q == NULL || q->head == NULL) {
+    free(q);
+    return;
+  }
+
+  list_ele_t* prev = q->head;
+  list_ele_t* curr = prev;
+  while (curr != NULL) {
+    curr = curr->next;
+    free(prev);
+    prev = curr;
+  }
+  free(q);
 }
 
 /*
@@ -37,23 +59,22 @@ void q_free(queue_t *q)
   Return true if successful.
   Return false if q is NULL or could not allocate space.
  */
-bool q_insert_head(queue_t *q, int v)
+bool q_insert_head(queue_t* q, int v)
 {
-    /* What should you do if the q is NULL? */
-    /* What if malloc returned NULL? */
-    return true;
+  /* What should you do if the q is NULL? */
+  /* What if malloc returned NULL? */
+  return true;
 }
-
 
 /*
   Attempt to insert element at tail of queue.
   Return true if successful.
   Return false if q is NULL or could not allocate space.
  */
-bool q_insert_tail(queue_t *q, int v)
+bool q_insert_tail(queue_t* q, int v)
 {
-    /* Remember: It should operate in O(1) time */
-    return false;
+  /* Remember: It should operate in O(1) time */
+  return false;
 }
 
 /*
@@ -63,19 +84,19 @@ bool q_insert_tail(queue_t *q, int v)
   If vp non-NULL and element removed, store removed value at *vp.
   Any unused storage should be freed
 */
-bool q_remove_head(queue_t *q, int *vp)
+bool q_remove_head(queue_t* q, int* vp)
 {
-    return true;
+  return true;
 }
 
 /*
   Return number of elements in queue.
   Return 0 if q is NULL or empty
  */
-int q_size(queue_t *q)
+int q_size(queue_t* q)
 {
-    /* Remember: It should operate in O(1) time */
-    return 0;
+  /* Remember: It should operate in O(1) time */
+  return q->size;
 }
 
 /*
@@ -85,8 +106,6 @@ int q_size(queue_t *q)
   calling q_insert_head or q_remove_head).  Instead, it should modify
   the pointers in the existing data structure.
  */
-void q_reverse(queue_t *q)
+void q_reverse(queue_t* q)
 {
-
 }
-
